6dd24e8f63aa6d132b5de067f41fbf316e729894
Kaushik Ganguly <Kaushik_Ganguly@comcast.com>
2018-08-05 12:23:58 -0400

    Action creators

--- index.js
+++ index.js
@@ -33,6 +33,41 @@ const TOGGLE_TODO = 'TOGGLE_TODO';
 const ADD_GOAL = 'ADD_GOAL';
 const REMOVE_GOAL = 'REMOVE_GOAL';
 
+function addTodoAction(todo) {
+    return {
+        type: ADD_TODO,
+        todo,
+    };
+}
+
+function removeTodoAction(id) {
+    return {
+        type: REMOVE_TODO,
+        id,
+    };
+}
+
+function toggleTodoAction(id) {
+    return {
+        type: TOGGLE_TODO,
+        id,
+    };
+}
+
+function addGoalAction(goal) {
+    return {
+        type: ADD_GOAL,
+        goal,
+    };
+}
+
+function removeGoalAction(id) {
+    return {
+        type: REMOVE_GOAL,
+        id,
+    };
+}
+
 function todos(state = [], action) {
     switch (action.type) {
         case ADD_TODO:
@@ -60,7 +95,7 @@ function goals(state = [], action) {
         case REMOVE_GOAL:
             return state.filter(goal => goal.id !== action.id);
         default:
-            state;
+            return state;
     }
 }
 
@@ -76,60 +111,46 @@ store.subscribe(() => {
     window.console.log('The new state is:', store.getState());
 });
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
+store.dispatch(
+    addTodoAction({
         id: 0,
         name: 'Walk the dog',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
+store.dispatch(
+    addTodoAction({
         id: 1,
         name: 'Wash the car',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
+store.dispatch(
+    addTodoAction({
         id: 2,
         name: 'Go to the gym',
         complete: true,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: REMOVE_TODO,
-    id: 1,
-});
+store.dispatch(removeTodoAction(1));
 
-store.dispatch({
-    type: TOGGLE_TODO,
-    id: 0,
-});
+store.dispatch(toggleTodoAction(0));
 
-store.dispatch({
-    type: ADD_GOAL,
-    goal: {
+store.dispatch(
+    addGoalAction({
         id: 0,
         name: 'Learn Redux',
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: ADD_GOAL,
-    goal: {
+store.dispatch(
+    addGoalAction({
         id: 1,
         name: 'Lose 20 pounds',
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: REMOVE_GOAL,
-    id: 0,
-});
+store.dispatch(removeGoalAction(0));
