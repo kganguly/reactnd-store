bc7e4b45b37388909b649829fbf1f9572ee5a492
Kaushik Ganguly <Kaushik_Ganguly@comcast.com>
2018-08-05 12:05:33 -0400

    Action contants

--- index.js
+++ index.js
@@ -27,13 +27,19 @@ function createStore(reducer) {
 }
 
 // App Code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 function todos(state = [], action) {
     switch (action.type) {
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo]);
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
             return state.filter(todo => todo.id !== action.id);
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map(
                 todo =>
                     todo.id === action.id
@@ -49,9 +55,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
     switch (action.type) {
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal]);
-        case 'REMOVE_GOAL':
+        case REMOVE_GOAL:
             return state.filter(goal => goal.id !== action.id);
         default:
             state;
@@ -71,7 +77,7 @@ store.subscribe(() => {
 });
 
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
         id: 0,
         name: 'Walk the dog',
@@ -80,7 +86,7 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
         id: 1,
         name: 'Wash the car',
@@ -89,7 +95,7 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
         id: 2,
         name: 'Go to the gym',
@@ -98,17 +104,17 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'REMOVE_TODO',
+    type: REMOVE_TODO,
     id: 1,
 });
 
 store.dispatch({
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id: 0,
 });
 
 store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal: {
         id: 0,
         name: 'Learn Redux',
@@ -116,7 +122,7 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal: {
         id: 1,
         name: 'Lose 20 pounds',
@@ -124,6 +130,6 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'REMOVE_GOAL',
+    type: REMOVE_GOAL,
     id: 0,
 });
